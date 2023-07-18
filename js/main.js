129109ddcdf1d524d091a9f9dd3bf00b0f7cb378
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-07-18 14:26:00 +0300

    fixed

--- js/main.js
+++ js/main.js
@@ -18,3 +18,28 @@ const reviewsSlider = new Swiper(".reviews-slider", {
     prevEl: ".reviews-slider__button--prev",
   },
 });
+// Добавляем обработчик события keyup для переключения слайдеров с помощью клавиатуры
+document.addEventListener("keyup", function (event) {
+  // Проверяем, нажата ли клавиша стрелка влево
+  if (event.key === "ArrowLeft") {
+    // Проверяем, активен ли слайдер отзывов
+    if (reviewsSlider.el.contains(document.activeElement)) {
+      // Если активен слайдер отзывов, переключаем его на предыдущий слайд
+      reviewsSlider.slidePrev();
+    } else {
+      // Если активен слайдер отелей, переключаем его на предыдущий слайд
+      hotelSlider.slidePrev();
+    }
+  }
+  // Проверяем, нажата ли клавиша стрелка вправо
+  else if (event.key === "ArrowRight") {
+    // Проверяем, активен ли слайдер отзывов
+    if (reviewsSlider.el.contains(document.activeElement)) {
+      // Если активен слайдер отзывов, переключаем его на следующий слайд
+      reviewsSlider.slideNext();
+    } else {
+      // Если активен слайдер отелей, переключаем его на следующий слайд
+      hotelSlider.slideNext();
+    }
+  }
+});
